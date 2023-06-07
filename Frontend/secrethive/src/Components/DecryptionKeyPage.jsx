a10eb27612217b44d6545b99a331800dbcc9c316
ARTHI R <arthiaaru23@gmail.com>
2023-06-07 18:22:16 +0530

    Initialised React, mui and implemented routers

---
+++ Frontend/secrethive/src/Components/DecryptionKeyPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DecryptionKeyPage = () => {
+  return (
+    <div>DecryptionKeyPage</div>
+  )
+}
+
+export default DecryptionKeyPage
---
+++ Frontend/secrethive/src/Components/EncryptionKeyPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EncryptionKeyPage = () => {
+  return (
+    <div>EncryptionKeyPage</div>
+  )
+}
+
+export default EncryptionKeyPage
