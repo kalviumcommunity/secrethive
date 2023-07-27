dc517bbe664bea272fe00adc33653856fe5b66b1
ARTHI R <arthiaaru23@gmail.com>
2023-07-27 16:43:50 +0530

    Created Signup component

--- Frontend/secrethive/src/Pages/About.jsx
+++ Frontend/secrethive/src/Pages/About.jsx
@@ -13,21 +13,23 @@ function About() {
           <img src={Logo}/>
         </div>
         <div className='subdiv2'>
-          <p>SecretHive is a powerful and reliable website that provides advanced encryption and decryption services for individuals and businesses. With SecretHive, you can securely store and transmit sensitive data, ensuring that it remains confidential and protected from unauthorized access.</p>
-          <p> One of the key features of SecretHive is its ability to encrypt and decrypt data both with and without a secret key. This means that you can use SecretHive to encrypt your data with a password or secret key, which only you and authorized parties have access to. Alternatively, you can also encrypt your data without a secret key, which means that the encryption algorithm used by SecretHive generates a random key that is stored securely on the website's servers.</p>
-          <p>Whether you choose to use a secret key or not, SecretHive guarantees strong encryption that is virtually impossible to break. This means that your data is protected even if it falls into the wrong hands. With SecretHive, you can have peace of mind knowing that your sensitive information is safe and secure.</p>
-          <p>In addition to its encryption and decryption services, SecretHive also provides a user-friendly interface that makes it easy to use and access your data. Whether you need to store files, exchange messages, or manage passwords, SecretHive has you covered. Try SecretHive today and experience the benefits of advanced encryption and decryption technology.</p>
-          <p><b>What is Encryption ?</b><br/>
-          The process of converting plaintext or readable data into a coded or scrambled form known as cipher-text, to protect it from unauthorized access or interception is called encryption.</p>
-          <p><b> Why Encryption?</b><br/>
-           Encryption is a critical component of cybersecurity and data privacy, as it helps to protect against data breaches, cyber attacks, and other forms of unauthorized access to sensitive information.</p>
-          <p> Encryption is used to secure a wide range of sensitive data, including financial transactions, personal information, and confidential communications.</p>
-          <p><b>What is Decryption ?</b><br/>
-          The process of converting the encrypted or ciphertext data back into its original plaintext or readable form is called decryption.</p>
-          <p> <b>Why Decryption?</b><br/>
-          Decryption is also used in digital signatures and certificate-based encryption systems, where a trusted third-party authority verifies the identity of the sender and ensures that the encrypted data is protected against tampering or interception.</p>
-          <p id='lastsent'><b>Why Encrypt your data more securely?</b><br/>
-          It's important to note that while encryption and decryption can provide an additional layer of security for sensitive data, they are not foolproof. There are always potential vulnerabilities that can be exploited by attackers, so it's important to use encryption and decryption in combination with other security measures, such as firewalls, anti-virus software, and strong authentication protocols.</p>
+          <div id='subdiv23'>
+            <p>SecretHive is a powerful and reliable website that provides advanced encryption and decryption services for individuals and businesses. With SecretHive, you can securely store and transmit sensitive data, ensuring that it remains confidential and protected from unauthorized access.</p>
+            <p> One of the key features of SecretHive is its ability to encrypt and decrypt data both with and without a secret key. This means that you can use SecretHive to encrypt your data with a password or secret key, which only you and authorized parties have access to. Alternatively, you can also encrypt your data without a secret key, which means that the encryption algorithm used by SecretHive generates a random key that is stored securely on the website's servers.</p>
+            <p>Whether you choose to use a secret key or not, SecretHive guarantees strong encryption that is virtually impossible to break. This means that your data is protected even if it falls into the wrong hands. With SecretHive, you can have peace of mind knowing that your sensitive information is safe and secure.</p>
+            <p>In addition to its encryption and decryption services, SecretHive also provides a user-friendly interface that makes it easy to use and access your data. Whether you need to store files, exchange messages, or manage passwords, SecretHive has you covered. Try SecretHive today and experience the benefits of advanced encryption and decryption technology.</p>
+            <p><b>What is Encryption ?</b><br/>
+            The process of converting plaintext or readable data into a coded or scrambled form known as cipher-text, to protect it from unauthorized access or interception is called encryption.</p>
+            <p><b> Why Encryption?</b><br/>
+            Encryption is a critical component of cybersecurity and data privacy, as it helps to protect against data breaches, cyber attacks, and other forms of unauthorized access to sensitive information.</p>
+            <p> Encryption is used to secure a wide range of sensitive data, including financial transactions, personal information, and confidential communications.</p>
+            <p><b>What is Decryption ?</b><br/>
+            The process of converting the encrypted or ciphertext data back into its original plaintext or readable form is called decryption.</p>
+            <p> <b>Why Decryption?</b><br/>
+            Decryption is also used in digital signatures and certificate-based encryption systems, where a trusted third-party authority verifies the identity of the sender and ensures that the encrypted data is protected against tampering or interception.</p>
+            <p id='lastsent'><b>Why Encrypt your data more securely?</b><br/>
+            It's important to note that while encryption and decryption can provide an additional layer of security for sensitive data, they are not foolproof. There are always potential vulnerabilities that can be exploited by attackers, so it's important to use encryption and decryption in combination with other security measures, such as firewalls, anti-virus software, and strong authentication protocols.</p>
+          </div>
         </div>
       </div>
       <><Footer /></>
--- Frontend/secrethive/src/Pages/Signup.jsx
+++ Frontend/secrethive/src/Pages/Signup.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import SideDiv from '../Components/SideDiv'
+import '../Styles/Signup.css'
 
 const Signup = () => {
   return (
-    <div>
-      <SideDiv/>
+    <div className='siginupmaindiv'>
+        <SideDiv/>
+      <div className='signupsubdiv2'>
+        <div className='signupdiv21'><p>Sign up!</p></div>
+        <div className='signupdiv22'>
+          <form>
+            <label for="nameinp">Name:</label><br/>
+            <input type="text" id="nameinp" placeholder='Enter your name' required/><br/>
+            <label for="emailinp">Email:</label><br/>
+            <input type="email" id="emailinp" placeholder='Enter your email' required/><br/>
+            <label for="passwordinp">New password:</label><br/>
+            <input type="password" id="passwordinp" placeholder='Enter new password' required/><br/>
+            <label for="repasswordinp">Re-enetr password:</label><br/>
+            <input type="password" id="repasswordinp" placeholder='Re-enter the password' required/><br/>
+            <button type='submit'> Sign up</button>
+          </form>
+        </div>
+        <div className='signupsubdiv3'>
+          <p>Already have an account?<b>Sign in!</b></p>
+      </div>
+      </div>
     </div>
   )
 }
